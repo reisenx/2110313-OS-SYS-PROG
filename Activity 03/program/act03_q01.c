99a81ff51e50630535391d2348009fe900f75bd6
reisenx <bloomrock555@gmail.com>
2025-01-25 15:38:13 +0700

    feat: act03_q01

---
+++ Activity 03/program/act03_q01.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+	int pid1, pid2;
+
+	pid1 = fork();
+	if(pid1 == 0) {
+		printf("I am the child process. My PID is %d ", getpid());
+		printf("and my parent's PID is %d\n", getppid());
+
+		pid2 = fork();
+		if(pid2 == 0) {
+			childpid = getppid();
+			grandchildpid = getpid();
+			printf("I am the grandchild process. My PID is %d ", getpid());
+			printf("and my parent's PID is %d\n", getppid());
+		}
+	}
+	else {
+		printf("I am the parent process. My PID is %d\n", getpid());
+	}
+	return 0;
+}
