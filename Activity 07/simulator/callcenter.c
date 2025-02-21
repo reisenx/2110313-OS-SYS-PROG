e74b8e4e0ad1c7e3ecf12c6dd477f8c64e292844
reisenx <bloomrock555@gmail.com>
2025-02-21 22:06:46 +0700

    feat: act06 initialization

---
+++ Activity 07/simulator/callcenter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+int main(int argc, char **argv) {
+	int num_agents = 2;
+	if(argc > 1)
+		num_agents = atoi(argv[1]);
+	printf("Starting a call center with %d agents.\n", num_agents);
+
+	//
+	// OS -- CRAETE NAMED SEMAPHORE HERE
+	//
+
+	int semval;
+	while(1) {
+
+		//
+		// OS -- PLACE CURRENT VALUE OF SEMAPHORE IN 'semval' HERE
+		//
+
+		printf("There are %d agents available now.\n", semval);
+		sleep(3);
+	}
+}
+
